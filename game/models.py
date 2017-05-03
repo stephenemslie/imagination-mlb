65c21f3eed72c768e1779389fef88b7b0cdb3c0a
Stephen Emslie <stephenemslie@gmail.com>
2017-05-03 13:04:21 +0200

    Added mobile_number field to User.

--- game/models.py
+++ game/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 class User(AbstractUser):
-    pass
+    mobile_number = PhoneNumberField()
