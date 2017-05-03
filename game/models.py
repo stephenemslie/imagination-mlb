32bf98f21baa1cb91a8ccbf6b8a5aa6ab22654e5
Stephen Emslie <stephenemslie@gmail.com>
2017-05-03 13:28:56 +0200

    Added game model.

--- game/models.py
+++ game/models.py
@@ -5,3 +5,10 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 class User(AbstractUser):
     mobile_number = PhoneNumberField()
+
+
+class Game(models.Model):
+    user = models.ForeignKey(User)
+    date = models.DateField(auto_now_add=True)
+    total_distance = models.IntegerField()
+    num_homeruns = models.IntegerField()
