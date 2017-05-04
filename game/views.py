60e4cd16641440b94ad0dfe5b5b83e27d644605d
Stephen Emslie <stephenemslie@gmail.com>
2017-05-04 21:39:31 +0200

    Added handlers for state changes.

--- game/views.py
+++ game/views.py
@@ -1,6 +1,9 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.decorators import detail_route
+
 from .models import User, Game
-from .serializers import UserSerializer, GameSerializer
+from .serializers import UserSerializer, GameSerializer, GameScoreSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -11,3 +14,39 @@ class UserViewSet(viewsets.ModelViewSet):
 class GameViewSet(viewsets.ModelViewSet):
     queryset = Game.objects.all()
     serializer_class = GameSerializer
+
+    @detail_route(methods=['POST'])
+    def confirm(self, request, pk=None):
+        game = self.get_object()
+        game.confirm()
+        game.save()
+        serializer = self.get_serializer(game)
+        return Response(serializer.data)
+
+    @detail_route(methods=['POST'])
+    def queue(self, request, pk=None):
+        game = self.get_object()
+        game.queue()
+        game.save()
+        serializer = self.get_serializer(game)
+        return Response(serializer.data)
+
+    @detail_route(methods=['POST'])
+    def play(self, request, pk=None):
+        game = self.get_object()
+        game.play()
+        game.save()
+        serializer = self.get_serializer(game)
+        return Response(serializer.data)
+
+    @detail_route(methods=['POST'])
+    def complete(self, request, pk=None):
+        game = self.get_object()
+        serializer = GameScoreSerializer(data=request.data)
+        if serializer.is_valid():
+            game.complete(**serializer.data)
+            game.save()
+            serializer = self.get_serializer(game)
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
