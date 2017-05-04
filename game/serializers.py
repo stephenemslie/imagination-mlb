abbd8515760cded8e3a08fbec37922c3e55f5c9c
Stephen Emslie <stephenemslie@gmail.com>
2017-05-04 21:38:46 +0200

    Added a game score serializer and user creation method

--- game/serializers.py
+++ game/serializers.py
@@ -12,7 +12,23 @@ class GameSerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
 
     games = GameSerializer(many=True, read_only=True)
+    active_game = GameSerializer(read_only=True)
 
     class Meta:
         model = User
         fields = ('id', 'first_name', 'last_name', 'mobile_number', 'email', 'games', 'active_game')
+
+    def create(self, validated_data):
+        validated_data['username'] = validated_data['mobile_number']
+        user = super().create(validated_data)
+        game = Game.objects.create(user=user)
+        user.active_game = game
+        user.save()
+        return user
+
+
+class GameScoreSerializer(serializers.Serializer):
+
+    score = serializers.IntegerField()
+    distance = serializers.IntegerField()
+    homeruns = serializers.IntegerField()
