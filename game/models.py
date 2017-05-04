68bfcaf4c61ca7ee9a83ea54d5a1a7713c75fe52
Stephen Emslie <stephenemslie@gmail.com>
2017-05-04 15:07:12 +0200

    Squahed model migrations.

--- game/models.py
+++ game/models.py
@@ -1,10 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+
+from django_fsm import FSMField, transition
 from phonenumber_field.modelfields import PhoneNumberField
 
 
+class Team(models.Model):
+    name = models.CharField(max_length=128)
+
+
 class User(AbstractUser):
-    mobile_number = PhoneNumberField()
+    mobile_number = PhoneNumberField(unique=True)
+    image = models.ImageField(null=True, blank=True)
+    is_finalist = models.BooleanField(default=False)
+    state = FSMField(default='registered')
+    team = models.ForeignKey(Team, null=True, blank=True)
 
 
 class Game(models.Model):
