fb415f4678a1840d17309efc5eab9c4c3aad9b0e
Stephen Emslie <stephenemslie@gmail.com>
2017-05-08 16:59:36 +0200

    Added Game and Team to admin

--- game/admin.py
+++ game/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import User, Game, Team
 
 admin.site.register(User, UserAdmin)
+admin.site.register(Game)
+admin.site.register(Team)
