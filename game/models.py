fdb186cd3e3acb52c1788fb4e4b6b78d0e6ae37d
Stephen Emslie <stephenemslie@gmail.com>
2017-05-08 17:00:32 +0200

    Added a string representation to Team

--- game/models.py
+++ game/models.py
@@ -13,6 +13,9 @@ from phonenumber_field.modelfields import PhoneNumberField
 class Team(models.Model):
     name = models.CharField(max_length=128, unique=True)
 
+    def __str__(self):
+        return self.name
+
 
 class User(AbstractUser):
     mobile_number = PhoneNumberField(unique=True)
