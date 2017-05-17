d4514f0e590d6b07dae779d70d79200a315aa1cc
Stephen Emslie <stephenemslie@gmail.com>
2017-05-17 09:51:35 +0200

    Moved wait-for-it checks to the first commands.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,14 +6,14 @@ if [ "$1" = 'test' ]; then
 fi
 
 if [ "$1" = 'runserver' ]; then
-    python3 manage.py migrate --noinput
-    exec bin/wait-for-it.sh postgres:5432 -- python3 manage.py runserver 0:8000
+    bin/wait-for-it.sh postgres:5432 -- python3 manage.py migrate --noinput
+    python3 manage.py runserver 0:8000
 fi
 
 if [ "$1" = 'gunicorn' ]; then
-    python3 manage.py migrate --noinput
+    bin/wait-for-it.sh postgres:5432 -- python3 manage.py migrate --noinput
     python3 manage.py collectstatic --noinput
-    exec bin/wait-for-it.sh postgres:5432 -- gunicorn mlb.wsgi
+    exec gunicorn mlb.wsgi
 fi
 
 if [ "$1" = 'shell' ]; then
