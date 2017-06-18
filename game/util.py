34034dd6f7d24696dc1920125155b437d2992d05
Stephen Emslie <stephenemslie@gmail.com>
2017-06-18 14:38:08 +0100

    Added basic docstrings

--- game/util.py
+++ game/util.py
@@ -4,12 +4,17 @@ import environ
 
 
 class Env(environ.Env):
+    """A django-environ variant for docker secrets."""
 
     def __init__(self, secrets_path, *args, **kwargs):
         self.secrets_path = secrets_path
         super().__init__(*args, **kwargs)
 
     def get_value(self, value, **kwargs):
+        """Return value for a given environment variable.
+        
+        Allow files in `secrets_path` to take priority over environment variables.
+        """
         path = os.path.join(self.secrets_path, value)
         if os.path.exists(path):
             return open(path).read()
