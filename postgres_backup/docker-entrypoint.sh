5a303f56285e8bd878e0ee323568b263b63781fe
Stephen Emslie <stephenemslie@gmail.com>
2017-06-18 16:23:07 +0100

    Added our of entrypoint for postgres backup

---
+++ postgres_backup/docker-entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -e
+export POSTGRES_PASSWORD=`cat /run/secrets/POSTGRES_PASSWORD`
+export S3_SECRET_ACCESS_KEY=`cat /run/secrets/S3_SECRET_ACCESS_KEY`
+export S3_ACCESS_KEY_ID=`cat /run/secrets/S3_ACCESS_KEY_ID`
+
+if [ "$1" = 'run' ]; then
+    ./run.sh
+fi
+
+exec "$@"
