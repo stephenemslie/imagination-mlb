1aae914db9bf1f6c6e2c2a9d13cbe62b11c33f5c
Stephen Emslie <stephenemslie@gmail.com>
2017-06-20 20:24:32 +0100

    Run with sh

--- postgres_backup/docker-entrypoint.sh
+++ postgres_backup/docker-entrypoint.sh
@@ -5,7 +5,7 @@ export S3_SECRET_ACCESS_KEY=`cat /run/secrets/S3_SECRET_ACCESS_KEY`
 export S3_ACCESS_KEY_ID=`cat /run/secrets/S3_ACCESS_KEY_ID`
 
 if [ "$1" = 'run' ]; then
-    ./run.sh
+    sh run.sh
 fi
 
 exec "$@"
