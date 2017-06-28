5ababb52141d6cb35f7728dca60770a44a71b516
Stephen Emslie <stephenemslie@gmail.com>
2017-06-28 20:58:42 +0200

    serve media in development

--- mlb/urls.py
+++ mlb/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import url
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
 from rest_framework import routers
 from rest_framework_jwt.views import obtain_jwt_token
@@ -10,6 +12,9 @@ urlpatterns = [
     url(r'^token/', obtain_jwt_token),
     url(r'^lighting/', set_lighting),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
