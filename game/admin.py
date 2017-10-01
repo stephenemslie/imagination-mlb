b99afcfcb8e217151855c22ae3efab2ceca15741
Stephen Emslie <stephenemslie@gmail.com>
2017-10-02 01:30:34 +0200

    Register Show with admin

--- game/admin.py
+++ game/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User, Game, Team
+from .models import User, Game, Team, Show
 
 admin.site.register(User, UserAdmin)
 admin.site.register(Game)
 admin.site.register(Team)
+admin.site.register(Show)
