d6b97eeaae5cad0a9e27ff8de6bbe08d5dbdaf04
Stephen Emslie <stephenemslie@gmail.com>
2017-10-01 16:31:17 +0200

    Moved active_recalls and next_recalls onto user queryset

--- game/models.py
+++ game/models.py
@@ -4,7 +4,7 @@ from django.db import models
 from django.db.models import Count
 from django.db.models.functions import Trunc
 from django.conf import settings
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, UserManager
 from django.utils import timezone
 
 from django_fsm import FSMField, transition
@@ -38,6 +38,25 @@ class Team(models.Model):
         return list(query)
 
 
+class UserQuerySet(models.QuerySet, UserManager):
+
+    def active_recalls(self, recall_expire=None, now=None):
+        now = now or timezone.now()
+        recall_expire = recall_expire or settings.RECALL_WINDOW_MINUTES
+        expire_time = now - datetime.timedelta(minutes=recall_expire)
+        query = self.filter(active_game__state='recalled',
+                            active_game__date_updated__gt=expire_time)
+        return query
+
+    def next_recalls(self, max_recalls=None):
+        max_recalls = max_recalls or settings.RECALL_WINDOW_SIZE
+        size = max(max_recalls - self.active_recalls().count(), 0)
+        query = self.order_by('active_game__date_created')\
+                    .filter(active_game__state='queued')\
+                    .exclude(mobile_number='')
+        return query[:size]
+
+
 class User(AbstractUser):
     mobile_number = PhoneNumberField(blank=True, default='')
     image = models.ImageField(null=True, blank=True)
@@ -48,6 +67,8 @@ class User(AbstractUser):
                                   choices=(('L', 'Left'), ('R', 'Right')),
                                   null=True, blank=True)
     signed_waiver = models.BooleanField(default=False)
+
+    objects = UserQuerySet.as_manager()
 
     def send_welcome_sms(self):
         message = ("Welcome to #mlbbattlegrounds. We’ll text you when it’s your "
@@ -60,24 +81,6 @@ class User(AbstractUser):
                    "Please head over to the batting cage where we’re waiting for "
                    "you. Do your team proud!")
         send_sms.delay(self.mobile_number.as_e164, message)
-
-
-class GameQuerySet(models.QuerySet):
-
-    def active_recalls(self, recall_expire=None, now=None):
-        now = now or timezone.now()
-        recall_expire = recall_expire or settings.RECALL_WINDOW_MINUTES
-        expire_time = now - datetime.timedelta(minutes=recall_expire)
-        query = self.filter(state='recalled', date_updated__gt=expire_time)
-        return query
-
-    def next_recalls(self, max_recalls=None):
-        max_recalls = max_recalls or settings.RECALL_WINDOW_SIZE
-        size = max(max_recalls - self.active_recalls().count(), 0)
-        query = self.order_by('date_created')\
-                    .filter(state='queued')\
-                    .exclude(user__mobile_number='')
-        return query[:size]
 
 
 class Game(models.Model):
@@ -96,8 +99,6 @@ class Game(models.Model):
     score = models.IntegerField(default=0)
     state = FSMField(default='new')
     souvenir_image = models.ImageField(upload_to='souvenirs/', null=True, blank=True)
-
-    objects = GameQuerySet.as_manager()
 
     @transition(field=state, source=['recalled', 'new'], target='queued')
     def queue(self):
